5bfed1afe34a4dd75289dcc5e00e741f8ecd85bc
jch <jiangchunhua@kuaidihelp.com>
2017-12-08 16:35:01 +0800

    composer-car

---
+++ src/Ford/Focus/Focus2013.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape;
+
+class Fiesta2013
+{
+    public static function info()
+    {
+        echo "This is Ford Fiesta2013! <br />";
+    }
+}
---
+++ src/Ford/Fusion/Fusion2013.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape;
+
+class Fusion2013
+{
+    public static function info()
+    {
+        echo "This is Ford Fusion2013! <br />";
+    }
+}
